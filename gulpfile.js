23acafdf6c705b125a938f6d79fc2b266528bff2
Chris Caragianis <chriscaragianis@gmail.com>
2016-05-22 17:31:25 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
+
+gulp.task('vendor-css', function() {
+  return gulp.src('./vendor/**/*.css')
+    .pipe(concatCss('styles/vendor.css'))
+    .pipe(gulp.dest('./public'));
+});
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/styles'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./vendor/**/*.css', ['vendor-css']);
+});
+
+gulp.task('default', ['sass',
+                      'sass:watch',
+                      'vendor-css',
+                     ], function() {});
